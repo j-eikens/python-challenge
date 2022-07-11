2af7b45999e739db6d232afcc8f398fc96ec0840
Joey Eikens <Joey.Eikens@gmail.com>
2022-07-11 14:06:28 -0500

    PyPoll - solution is ready except for printing format of dictionary. Per BSC recommendation, started new solution. This is commented out and still needs a lot of work

--- PyPoll/Solution/main.py
+++ PyPoll/Solution/main.py
@@ -3,6 +3,7 @@ import csv
 
 csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 
+#variables for approach 1
 candidates = []
 county = []
 ballot_ID = []
@@ -10,11 +11,74 @@ ballot_ID = []
 totalvotes = []
 percentagevotes = []
 
+# #variables for approach 2
+# candidates = []
+# county = []
+# ballot_ID = []
+
+# total_vote = []
+# percent_vote = []
+
+# unique_candidates = []
+
+
 with open(csvpath) as csvfile:
     
     election_data = csv.reader(csvfile, delimiter=',')
 
     csv_header = next(election_data)
+
+
+#--------------------------------------------------------------------------
+#approach 2
+
+#     votes = 0
+
+#     for row in election_data:
+
+#         votes += 1
+
+#         candidates.append(row[2])
+#         county.append(row[1])
+#         ballot_ID.append(row[0])
+
+#     #find unique candidates
+#     for candidate in candidates:
+        
+#         if candidate not in unique_candidates:
+            
+#             unique_candidates.append(candidate)
+
+    
+#     resultsdictionary = (unique_candidates)
+
+#     print(resultsdictionary)
+    
+
+
+#     # count = 1
+#     # #count votes for each candidate
+#     # for candidate in unique_candidates:
+
+#     #     if unique_candidates.value == candidates:
+
+#     #         count += 1
+
+#     # print(unique_candidates)
+#     # print(count)
+
+
+
+# #Results
+# print('Election Results')
+# print('------------------------')
+# print(f'Total Votes: {votes}')
+
+#-------------------------------------------------------------------------
+
+
+
+# approach 1 - more complex???
 
 # appending rows to variables.
     for row in election_data:
@@ -30,7 +94,7 @@ with open(csvpath) as csvfile:
     unique_list = (list(list_set))
 
 
-    #search for 
+    #find instances of unique candidates - BSC claims there is an easier way 
     counter = 0
 
     for row in unique_list: 
@@ -50,35 +114,44 @@ with open(csvpath) as csvfile:
     #create dictionary
     dictionary = {"Candidate": unique_list, "Total Votes": totalvotes, "Percentage Votes": percentagevotes}
 
-    print(dictionary)  
-    print("---------------------")
-    
-    # print(len(dictionary["Total Votes"]))
+    #trying to create different dictionary
+    dictionary2 = {"Charles Casper Stockham:": totalvotes[1], "Total Votes" : totalvotes}
 
+ 
     #Find who is has the most votes
     mostvotes = 0
     
-    for element in range(len(dictionary["Total Votes"])):  
+    #This returns maximum votes
+    for element in dictionary["Total Votes"]:  
         
         #print(element)
 
         if element > mostvotes:
 
             mostvotes = element
-            #print(element)
+            #print(mostvotes)
 
 
-    # for element in dictionary["Total Votes"]:  
-        
-    #     print(element)
+    #Finds winner
+    index = totalvotes.index(mostvotes)
 
-    #     if element > mostvotes:
+    winner = unique_list[index]
 
-    #         mostvotes = element
+    print('Election Results')
+    print('----------------------------')
+    print(f'Total Votes: {mostvotes}')
+    print('----------------------------')
+    print(dictionary)
+    print('----------------------------')
+    print(f'Winner:  {winner}')
+    print('----------------------------')
 
-    print('----------')  
-    print(element)      
-    print(mostvotes)
+    # for key, value in dictionary.items():
+    #     print(key, ":", value)
+
+    print(dictionary2)
+
+
 
 
 
